667e1a89e39385f1d9ddcb0287940ecd16a7fa87
Monalisa Panda <monalisapanda89@gmail.com>
2023-03-08 22:52:43 -0800

    Added TestGit file

---
+++ src/p1/GitTestFile.java
@@ -0,0 +1,24 @@
+package p1;
+
+public class GitTestFile {
+
+	public static void main(String[] args) {
+		 System.out.println("Hello world");
+		 int x=0;
+		 System.out.println(x);
+		 float y=1.2f;
+		 char z='A';
+		 boolean var1 =true;
+		 String str="my string";
+		 System.out.println(str.toUpperCase());
+		 System.out.println(str.contains("my1"));
+		 System.out.println(str.length());
+		 System.out.println(str.indexOf("t"));
+		 System.out.println(str.charAt(4));
+		 System.out.println(str.endsWith("nng"));
+		 System.out.println(str.replace("ing", "klm"));
+		 
+
+	}
+
+}
